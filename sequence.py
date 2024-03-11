8c46fbb26d1dff2a667f34a86281f9ccdf418ea1
bansikandoliya10 <155720362+bansikandoliya10@users.noreply.github.com>
2024-03-11 16:01:00 +0530

    Add files via upload

---
+++ sequence.py
@@ -0,0 +1,9 @@
+course=["PGDCA", "BSCIT", "BCA"]
+print ("list : ",course[1])
+
+course1=("pgdca", "bca", "bscit")
+print("Tuple : ",course1[1])
+
+course0="pgdca"
+print(course0[0])
+
---
+++ sets.py
@@ -0,0 +1,3 @@
+pythonset={"APPLE", "2nd APPLE", "APPLE", "3rd APPLE"}
+print(len(pythonset))
+print(pythonset)
